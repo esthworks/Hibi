eec91350fbd2e089613793bdc94f61ef39b23f72
esthworks <maury.esther@gmail.com>
2026-04-30 22:10:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ async function saveData() {
         .upsert([
             {
                 date: today,
-                data: todayData
+                data: todayData,
                 user_key: "nekonimbus"
             }
         ]);
